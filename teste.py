60440da2a76022bf5ea3da1e0d2fa06a3342fcd9
BRtransf <100425100+BRtransf@users.noreply.github.com>
2022-10-25 10:41:28 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-base = pd.concat(pd.read_csv('base1.csv',sep=';'),pd.read_csv('base2.csv',sep=';')).drop_duplicates(subset=['Jogador','Equipa'])
+base = pd.concat([pd.read_csv('base1.csv',sep=';'),pd.read_csv('base2.csv',sep=';')]).drop_duplicates()
 
 st.write(len(base))
