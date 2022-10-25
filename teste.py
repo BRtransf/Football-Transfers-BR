64857e01d1725b692aaf683212901718aadef46f
BRtransf <100425100+BRtransf@users.noreply.github.com>
2022-10-25 09:52:01 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-data_url = ('https://github.com/BRtransf/Football-Transfers-BR/blob/main/base1.xlsx')
+data_url = ("https://github.com//BRtransf/Football-Transfers-BR/blob/main/base1.xlsx?raw=true")
 
 base = pd.read_excel(data_url,engine='openpyxl')
 
