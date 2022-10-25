f94eb66fd0f0e760d259fd48165b9ccd2f39c934
BRtransf <100425100+BRtransf@users.noreply.github.com>
2022-10-25 09:40:07 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 data_url = ('https://github.com/BRtransf/Football-Transfers-BR/blob/main/base1.xlsx')
 
-base = pd.read_csv(data_url)
+base = pd.read_excel(data_url,engine=“openpyxl”)
 
 st.write(base)
