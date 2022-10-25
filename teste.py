7b91ac26f1e1a22b09afc3f9a2e34277668ed1dd
BRtransf <100425100+BRtransf@users.noreply.github.com>
2022-10-25 11:10:27 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,14 +3,6 @@ import pandas as pd
 
 base = pd.concat([pd.read_csv('base1.csv',sep=';'),pd.read_csv('base2.csv',sep=';')]).drop_duplicates(['Jogador','Equipa','Minutos jogados:'])
 
-def trat_base_wyscout(base):
-  for coluna in base.columns:
-    if type(base[coluna][0]) == 'int':
-      base[coluna] = base[coluna].astype('int')
-      
-trat_base_wyscout(base)
-
-base['DefesaMeta'] = (base['Golos sofridos esperados/90']/base['Golos sofridos/90'])*(base['Remates sofridos/90']*base['Defesas, %'])
       
 st.write(len(base))
 
@@ -32,4 +24,52 @@ idade_max = st.text_input('Idade máxima para analisar:')
 posicao = float(posicao)
 min_min = int(min_min)
 idade_max = int(idade_max)
+
+dic_posicoes = {1:['Minutos jogados:'],
+                2:['Minutos jogados:'],
+                3:['Minutos jogados:'],
+                4:['Minutos jogados:'],
+                5:['Minutos jogados:'],
+                6:['Minutos jogados:'],
+                7:['Minutos jogados:'],
+                8:['Minutos jogados:'],
+                8.5:['Minutos jogados:'],
+                9:['Minutos jogados:'],
+                9.5:['Minutos jogados:'],
+                10:['Minutos jogados:'],
+                11:['Minutos jogados:']}
+
+dic_siglas = {1:['GK'],
+              2:['RB'],
+              3:['CB','RCB'],
+              4:['CB','LCB'],
+              5:['RDMF','LDMF','DMF','LCMF','RCMF','CMF'],
+              6:['LB'],
+              7:['LW','LAMF','LWF'],
+              8:['RDMF','LDMF','DMF','LCMF','RCMF','CMF'],
+              8.5:['RDMF','LDMF','DMF','LCMF','RCMF','CMF','AMF','RAMF','LAMF'],
+              9:['CF'],
+              9.5:['CF','AMF','RAMF','LAMF'],
+              10:['AMF','RAMF','LAMF'],
+              11:['RW','RAMF','RWF']}
+
+
+lista_cols = ['Jogador','Equipa','Posição','Idade','Partidas jogadas','Minutos jogados:']
+
+for item in dic_posicoes[posicao]:
+    lista_cols.append(item)
+    
+
+df_resumo = base[lista_cols].copy()
+df_resumo = df_resumo[(df_resumo['Minutos jogados:'] >= min_min)&(df_resumo.Idade<=idade_max)].reset_index(drop=True)
+
+st.write(df_resumo)
+
+
+
+
+
+
+
+
 
