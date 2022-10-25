e6f3110ff6750b1732a0c8dd02f212c826ffd09a
BRtransf <100425100+BRtransf@users.noreply.github.com>
2022-10-25 09:31:51 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-base = pd.read_excel('https://github.com/BRtransf/Football-Transfers-BR/blob/main/base1.xlsx').append(pd.read_excel('https://github.com/BRtransf/Football-Transfers-BR/blob/main/base1.xlsx')).drop_duplicates()
+base = pd.read_excel('https://github.com/BRtransf/Football-Transfers-BR/base1.xlsx').append(pd.read_excel('https://github.com/BRtransf/Football-Transfers-BR/base1.xlsx')).drop_duplicates()
 
 st.write(base)
