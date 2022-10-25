756a488f7f180ea2ae511f321bbf60ac24a341b2
BRtransf <100425100+BRtransf@users.noreply.github.com>
2022-10-25 12:42:21 -0300

    Update comparar.py

--- comparar.py
+++ comparar.py
@@ -17,9 +17,9 @@ elif len(base[base.Jogador==nome_busca1]['Jogador'])>1:
   if len(base[(base.Jogador==nome_busca1)&(base.Equipa == clube1)]['Jogador'])>1:
     st.write(base[(base.Jogador==nome_busca1)&(base.Equipa == clube)])
     idade1 = int(st.input_text("Idade do primeiro jogador:")
-    st.write("Jogador selecionado: "+ nome_busca1+ clube1+str(idade1))
+    st.write( nome_busca1+ clube1+str(idade1))
                  
 else:
     idade1 = base[base.Jogador==nome_busca1]['Idade'].tolist()[0]
     clube1 = base[base.Jogador==nome_busca1]['Equipa'].tolist()[0]
-    st.write("Jogador selecionado: "+ nome_busca1+ clube1+str(idade1))
+    st.write(nome_busca1+ clube1+str(idade1))
