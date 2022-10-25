098634e473271391f1022341cb314a69ab185acb
BRtransf <100425100+BRtransf@users.noreply.github.com>
2022-10-25 10:58:42 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,9 +5,15 @@ base = pd.concat([pd.read_csv('base1.csv',sep=';'),pd.read_csv('base2.csv',sep='
 
 st.write(len(base))
 
-
+lista_pos = []
+for num in range(1,12):
+  lista_pos.append(str(num))
+lista_pos.append('8.5')
+lista_pos.append('9.5')
 
 posicao = st.text_input('Inserir o número da posição desejada (1 a 11):')
+if posicao not in lista_pos:
+  st.write('Favor inserir posição válida')
 
 min_min = st.text_input('Minutagem mínima para analisar:')
 
