58a10a4da6a008f454e55f538fb595c8940eeeb8
BRtransf <100425100+BRtransf@users.noreply.github.com>
2022-10-25 09:33:50 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-base = pd.read_excel('https://github.com/BRtransf/Football-Transfers-BR/base1.xlsx').append(pd.read_excel('https://github.com/BRtransf/Football-Transfers-BR/base1.xlsx')).drop_duplicates()
+base = pd.read_excel('base1.xlsx')
 
 st.write(base)
