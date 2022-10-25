5dd39d0f620ae7a5f6724cd1ecbaa3a5f85da0fd
BRtransf <100425100+BRtransf@users.noreply.github.com>
2022-10-25 12:45:46 -0300

    Update comparar.py

--- comparar.py
+++ comparar.py
@@ -6,17 +6,17 @@ base = pd.read_csv('BRA1-2018-1.csv',sep=';',decimal=',')
 
 st.write(base)
 
-nome_busca1 = st.input_text("Nome do primeiro jogador:")
+nome_busca1 = st.text_input("Nome do primeiro jogador:")
 
 if len(base[base.Jogador==nome_busca1]) == 0:
   st.write("Favor inserir o nome do jogador igual no WyScout)")
 
 elif len(base[base.Jogador==nome_busca1]['Jogador'])>1:
   st.write(base[base.Jogador==nome_busca1])
-  clube1 = st.input_text("Clube do primeiro jogador:")
+  clube1 = st.text_input("Clube do primeiro jogador:")
   if len(base[(base.Jogador==nome_busca1)&(base.Equipa == clube1)]['Jogador'])>1:
     st.write(base[(base.Jogador==nome_busca1)&(base.Equipa == clube)])
-    idade1 = int(st.input_text("Idade do primeiro jogador:"))
+    idade1 = int(st.text_input("Idade do primeiro jogador:"))
     base1 = base[(base.Jogador==nome_busca1)&(base.Equipa == clube1)&(base.Idade==idade1)]
     st.write(base1)
                  
