0a3e4491df5a4093448ac833d3dc60345f7704b1
BRtransf <100425100+BRtransf@users.noreply.github.com>
2022-10-25 12:49:16 -0300

    Update comparar.py

--- comparar.py
+++ comparar.py
@@ -15,7 +15,7 @@ elif len(base[base.Jogador==nome_busca1]['Jogador'])>1:
   st.write(base[base.Jogador==nome_busca1])
   clube1 = st.text_input("Clube do primeiro jogador:")
   if len(base[(base.Jogador==nome_busca1)&(base.Equipa == clube1)]['Jogador'])>1:
-    st.write(base[(base.Jogador==nome_busca1)&(base.Equipa == clube)])
+    st.write(base[(base.Jogador==nome_busca1)&(base.Equipa == clube1)])
     idade1 = int(st.text_input("Idade do primeiro jogador:"))
     base1 = base[(base.Jogador==nome_busca1)&(base.Equipa == clube1)&(base.Idade==idade1)]
     st.write(base1)
@@ -26,3 +26,26 @@ elif len(base[base.Jogador==nome_busca1]['Jogador'])>1:
 else:
     base1 = base[base.Jogador == nome_busca1]
     st.write(base1)
+
+    
+nome_busca2 = st.text_input("Nome do primeiro jogador:")
+
+if len(base[base.Jogador==nome_busca2]) == 0:
+  st.write("Favor inserir o nome do jogador igual no WyScout")
+
+elif len(base[base.Jogador==nome_busca2]['Jogador'])>1:
+  st.write(base[base.Jogador==nome_busca2])
+  clube2 = st.text_input("Clube do primeiro jogador:")
+  if len(base[(base.Jogador==nome_busca2)&(base.Equipa == clube2)]['Jogador'])>1:
+    st.write(base[(base.Jogador==nome_busca2)&(base.Equipa == clube2)])
+    idade2 = int(st.text_input("Idade do primeiro jogador:"))
+    base2 = base[(base.Jogador==nome_busca2)&(base.Equipa == clube2)&(base.Idade==idade2)]
+    st.write(base2)
+  else:
+    base1 = base[(base.Jogador==nome_busca2)&(base.Equipa == clube2)]
+    st.write(base2)
+                 
+else:
+    base2 = base[base.Jogador == nome_busca2]
+    st.write(base2)    
+    
