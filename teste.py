194c74601d8c4e0f12a59d2862104ff97655b5bd
BRtransf <100425100+BRtransf@users.noreply.github.com>
2022-10-25 10:32:36 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-base = pd.read_csv('base1.csv',sep=';')
+base = pd.read_csv('base1.csv',sep=';').append(pd.read_csv('base2.csv',sep=';')).drop_duplicates()
 
-st.write(base)
+st.write(len(base))
